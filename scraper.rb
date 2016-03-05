5def462a9484e96aaf8aa7f74645288ca97699ec
Tony Bowden <tony@tmtm.com>
2016-03-05 10:27:35 +0000

    Fetch from Wikipedia scraper

--- scraper.rb
+++ scraper.rb
@@ -1,10 +1,5 @@
 require 'wikidata/fetcher'
 
-names = EveryPolitician::Wikidata.wikipedia_xpath( 
-  url: 'https://en.wikipedia.org/wiki/Parliament_of_Nauru',
-  after: '//h2/span[@id="Current_MPs"]',
-  before: '//h2/span[@id="Speaker"]',
-  xpath: '//table//tr//td[position()=last()-3]//a[not(@class="new")]/@title',
-) 
+names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/nauru-parliament', column: 'wikiname')
 
 EveryPolitician::Wikidata.scrape_wikidata(names: { en: names }, output: false)
