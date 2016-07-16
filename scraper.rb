7f330b71b49151ab5133eabe068cacabe595499a
Tony Bowden <tony@tmtm.com>
2016-07-16 13:29:03 +0100

    Add from Category

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 require 'wikidata/fetcher'
 
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/nauru-parliament', column: 'wikiname')
+cat = WikiData::Category.new( 'Category:Members_of_the_Parliament_of_Nauru', 'en').member_titles
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names }, output: false)
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: names | cat }, output: false)
